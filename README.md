b74f392347e31677f98c7012273d38e6f933573a
Jesús Martínez <jesusmartinez@Jesuss-MacBook-Pro.local>
2018-07-13 18:11:19 -0500

    README and docs.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Keras + DL4J
+
+Example of a simple integration between Keras (running with a TensorFlo backend) and Deeplearning4J. The model is developed, tested and saved using Keras (details here). Then, it's loaded in DeepLearning4J from the corresponding model and weights files that resulted from the Keras export process, and run.
+
+# Installation
+
+The easiest way to get started is to load this project in IntelliJ. You'll also need `conda`.
+
+### Create conda environment
+
+`conda env create -f env.yml`
+`source activate data`
+
+### Run Keras model
+
+`cd src/main`
+`python keras_model.py`
+
+You should see how to model gets trained, and also two files should appear at the same level of the script: `iris_model_json` and `iris_model_save`
+
+### Load model in DL4J
+
+Just press play in the Dl4JModel class ;)
+
+# NOTE:
+
+- As of July of 2018, DL4J does not support Keras 2 export API. Hence, we're using Keras 1 API, which requires versions a bit older of Python and TensorFlow, but for the purposes of this example it shouldn't be an issue.
--- src/main/scala/Dl4JModel.scala
+++ src/main/scala/Dl4JModel.scala
@@ -6,28 +6,36 @@ import org.deeplearning4j.nn.modelimport.keras.KerasModelImport
 import org.nd4j.linalg.io.ClassPathResource
 
 object Dl4JModel extends App {
+  // Path to relevant files.
   val jsonModelPath = "/Users/jesusmartinez/Portfolio/keras-dl4j-integration/src/main/iris_model_json"
   val weightsPath = "/Users/jesusmartinez/Portfolio/keras-dl4j-integration/src/main/iris_model_save"
 
+  // Imported model
   val model = KerasModelImport.importKerasSequentialModelAndWeights(jsonModelPath, weightsPath)
 
+  // Define how we should interpret the data.
   val numberOfLinesToSkip = 0
   val delimiter = ','
   val recordReader = new CSVRecordReader(numberOfLinesToSkip, delimiter)
   recordReader.initialize(new FileSplit(new ClassPathResource("iris-encoded.data").getFile))
 
+  // Load the data in memory as it is small.
   val labelIndex = 4
-  val numClasses = 3
+  val numberOfClasses = 3
   val batchSize = 150
 
-  val dataSetIterator = new RecordReaderDataSetIterator(recordReader, batchSize, labelIndex, numClasses)
+  val dataSetIterator = new RecordReaderDataSetIterator(recordReader, batchSize, labelIndex, numberOfClasses)
 
   val data = dataSetIterator.next()
 
   data.shuffle()
 
-  val eval = new Evaluation(3)
+  // No training required. It was done by Keras.
+  // Let's evaluate the model.
+  val evaluator = new Evaluation(numberOfClasses)
   val outout = model.output(data.getFeatureMatrix)
-  eval.eval(data.getLabels, outout)
-  System.out.println(eval.stats())
+
+  evaluator.eval(data.getLabels, outout)
+
+  System.out.println(evaluator.stats())
 }
